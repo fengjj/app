eae611e1efbd3f5e1a2d54036c0d72356cf824c9
fengjj <fengjj@just-PC>
2015-12-19 14:42:27 +0800

    servlet 获取post参数，从stream里取~~~~

--- new_app/src/com/sitech/crmpd/MyServlet.java
+++ new_app/src/com/sitech/crmpd/MyServlet.java
@@ -1,22 +1,54 @@
 package com.sitech.crmpd;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.lf5.util.StreamUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.sitech.market.service.trading.inter.IHDMAct;
+
 public class MyServlet extends HttpServlet{
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		Enumeration<String> e = req.getAttributeNames();
-		System.out.println(e.hasMoreElements()+"  "+e.nextElement());
-		/*for(int i = 0 ;i <e.){
-			
-		}*/
-		resp.reset();
+		ServletInputStream sis = req.getInputStream();
+		byte[] b = StreamUtils.getBytes(sis);
+		String par = new String(b);
+		System.out.println(par);
+		System.out.println(getServletName());
+		System.out.println(req.getAttribute("aaa"));
+		System.out.println(req.getParameter("aaa"));
+		System.out.println(req.getParameterMap());
+		System.out.println(req.getLocalName());
+		System.out.println(req.getAuthType());
+		System.out.println(req.getContextPath());
+		
+		ApplicationContext ac1 = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+		ApplicationContext ac2 = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		IHDMAct h = (IHDMAct)ac1.getBean("hDMActSvc");  
+		if(h==null){
+			System.out.println(111);
+			h = (IHDMAct)ac2.getBean("hDMActSvc");  
+		}
+		
+		String ret = h.createSmpLotteryInfo(req.getPathInfo());
+		PrintWriter pw = resp.getWriter();
+		pw.append(ret);
+		pw.flush();
+		pw.close();
+		resp.flushBuffer();
 	}
 }
--- new_app/src/test/TestHttp.java
+++ new_app/src/test/TestHttp.java
@@ -6,7 +6,7 @@ import com.sitech.util.HttpUtil;
 public class TestHttp {
 
 	public static void main(String[] args) {
-		String result = HttpUtil.invoke("http://127.0.0.1:8080/new_app/myServlet/hdmact/createSmpLotteryInfo"	, "{aaa:bbb}");
+		String result = HttpUtil.invoke("http://127.0.0.1:8080/new_app/myServlet/hdmact/createSmpLotteryInfo?aaa=sss"	, "{aaa:bbb}");
 		System.out.println(result);
 		
 	}
